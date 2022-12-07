655f21a1caf1235fa756bb2747c1f25929ad11e5
Ricwol <115089602+Ricwol@users.noreply.github.com>
2022-12-07 15:49:24 +0000

    Create finger_exercise_01.py

---
+++ chapter_05/finger_exercise_01.py
@@ -0,0 +1,7 @@
+# Finger exercise, p.117
+# Write an expression that evaluates to the mean of a tuple of numbers.
+# Use the function sum.
+
+nums = (5, 3, 7, 123, 3, 4, 5,)
+mean = sum(nums) / len(nums)
+print(f"{mean = }")
